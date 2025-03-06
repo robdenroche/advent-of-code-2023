85b8d589c8883a08fa797b5eb1f799aedcacba7a
Robert E Denroche <rob.denroche@gmail.com>
2025-03-06 13:17:56 -0500

    feat: day09 part 2 solution

--- day09/day09.py
+++ day09/day09.py
@@ -149,3 +149,67 @@ with open("input.txt") as fh:
 
 
 print(sum)  # 1877825184
+
+
+"""
+Of course, it would be nice to have even more history included in your report.
+Surely it's safe to just extrapolate backwards as well, right?
+
+For each history, repeat the process of finding differences until the sequence
+of differences is entirely zero. Then, rather than adding a zero to the end and
+filling in the next values of each previous sequence, you should instead add a
+zero to the beginning of your sequence of zeroes, then fill in new first values
+for each previous sequence.
+
+In particular, here is what the third example history looks like when
+extrapolating back in time:
+
+5  10  13  16  21  30  45
+  5   3   3   5   9  15
+   -2   0   2   4   6
+      2   2   2   2
+        0   0   0
+
+Adding the new values on the left side of each sequence from bottom to top
+eventually reveals the new left-most history value: 5.
+
+Doing this for the remaining example data above results in previous values of
+-3 for the first history and 0 for the second history. Adding all three new
+values together produces 2.
+
+Analyze your OASIS report again, this time extrapolating the previous value for
+each history. What is the sum of these extrapolated values?
+"""
+
+
+def find_previous(hist: list) -> int:
+
+    # hist diffs will store the lists of differences
+    # with the most recent at position 0
+    hist_diffs = list()
+    hist_diffs.append(hist)
+
+    while not_zero(hist_diffs[0]):
+        hist_diffs.insert(0, get_diffs(hist_diffs[0]))
+
+    # subtract all the first values in the hist diff lists
+    next = 0
+    for i in range(1, len(hist_diffs)):
+        next = hist_diffs[i][0] - next
+
+    return next
+
+
+# running sum of next values
+sum = 0
+
+with open("input.txt") as fh:
+    for line in fh:
+        line = line.rstrip()
+
+        hist = [int(i) for i in line.split()]
+
+        sum += find_previous(hist)
+
+
+print(sum)  # 1108
