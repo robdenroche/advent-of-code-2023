3ef135a5715231a9be9f086e311bdc571e4c68a0
Robert E Denroche <rob.denroche@gmail.com>
2025-03-19 16:03:02 -0400

    feat: day10 solution part 2 - scanline algo!

--- day10/day10.py
+++ day10/day10.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 """
 Scanning the area, you discover that the entire field you're standing on is
 densely packed with pipes; it was hard to tell at first because they're the
@@ -247,3 +249,198 @@ while current != "S":
     current = pipes[i][j]
 
 print(steps / 2)  # 7030
+
+
+"""
+You quickly reach the farthest point of the loop, but the animal never emerges.
+Maybe its nest is within the area enclosed by the loop?
+
+To determine whether it's even worth taking the time to search for such a nest,
+you should calculate how many tiles are contained within the loop. For example:
+
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+The above loop encloses merely four tiles - the two pairs of . in the southwest
+and southeast (marked I below). The middle . tiles (marked O below) are not in
+the loop. Here is the same loop again with those regions marked:
+
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+In fact, there doesn't even need to be a full tile path to the outside for
+tiles to count as outside the loop - squeezing between pipes is also allowed!
+Here, I is still within the loop and O is still outside the loop:
+
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+In both of the above examples, 4 tiles are enclosed by the loop.
+
+Here's a larger example:
+
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+The above sketch has many random bits of ground, some of which are in the loop
+(I) and some of which are outside it (O):
+
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+In this larger example, 8 tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the
+loop. Here's another example with many bits of junk pipe lying around that
+aren't connected to the main loop at all:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+Here are just the tiles that are enclosed by the loop marked with I:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+In this last example, 10 tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the area
+within the loop. How many tiles are enclosed by the loop?
+
+"""
+
+# pipes is the 2d list of lists
+pipes = list()
+
+with open("input.txt") as fh:
+    for line in fh:
+        line = line.rstrip()
+
+        pipes.append(list(line))
+
+
+# initialize pipe map with 0s
+pipe_map = deepcopy(pipes)
+
+for i in range(0, len(pipe_map)):
+    for j in range(0, len(pipe_map[i])):
+        pipe_map[i][j] = "0"
+
+
+# starting from the S, walk along the pipe until we loop
+# and find the S again, changing each character to a "P"
+current = None
+direction = None
+(i, j) = find_start(pipes)
+
+while current != "S":
+    (i, j, direction) = find_next_pipe(pipes, i, j, direction)
+    current = pipes[i][j]
+    pipe_map[i][j] = current
+
+
+# give the scanline algorithm a go
+in_loop = False
+prev_elbow = None
+
+in_loop_count = 0
+
+"""
+| is a vertical pipe connecting north and south.
+- is a horizontal pipe connecting east and west.
+L is a 90-degree bend connecting north and east.
+J is a 90-degree bend connecting north and west.
+7 is a 90-degree bend connecting south and west.
+F is a 90-degree bend connecting south and east.
+. is ground; there is no pipe in this tile.
+S is the starting position (and happens to be a J)
+"""
+
+
+def toggle(in_loop):
+    if in_loop is True:
+        return False
+    else:
+        return True
+
+
+for i in range(0, len(pipe_map)):
+    in_loop = False  # should always be the case here
+    prev_elbow = None
+
+    for j in range(0, len(pipe_map[i])):
+        cur = pipe_map[i][j]
+        if cur == "S":
+            # the S is a J pipe in the input
+            cur = "J"
+
+        if cur == "0" and in_loop is True:
+            in_loop_count += 1
+
+        elif cur == "|":
+            in_loop = toggle(in_loop)
+
+        elif cur in ["L", "F"]:
+            prev_elbow = cur
+
+        elif cur == "J":
+            if prev_elbow == "L":
+                prev_elbow = None
+            elif prev_elbow == "F":
+                in_loop = toggle(in_loop)
+                prev_elbow = None
+
+        elif cur == "7":
+            if prev_elbow == "F":
+                prev_elbow = None
+            elif prev_elbow == "L":
+                in_loop = toggle(in_loop)
+                prev_elbow = None
+
+print(in_loop_count)  # 285
